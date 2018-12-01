58d61317b643da087abb768d9a8b9bcda47ca588
Ziad Hamdieh <ziad.hamdieh@td.com>
2018-12-01 16:08:21 -0500

    home VC

---
+++ LocalHackDay2018/LocalHackDay2018/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  LocalHackDay2018
+//
+//  Created by Ziad Hamdieh on 2018-12-01.
+//  Copyright © 2018 Jarvis Wu. All rights reserved.
+//
+
+import UIKit
+
+struct Movie {
+    private let title = String()
+    private let rating = Float()
+}
---
+++ LocalHackDay2018/LocalHackDay2018/View/MovieCell.swift
@@ -0,0 +1,45 @@
+//
+//  MovieCell.swift
+//  LocalHackDay2018
+//
+//  Created by Ziad Hamdieh on 2018-12-01.
+//  Copyright © 2018 Jarvis Wu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var containerView: UIView! {
+        didSet {
+            containerView.layer.cornerRadius = 10
+            containerView.layer.shadowOpacity = 0.4
+            containerView.layer.shadowRadius = 3
+            containerView.layer.shadowColor = UIColor.lightGray.cgColor
+        }
+    }
+    
+    @IBOutlet weak var movieSplash: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var genre: UILabel!
+    
+    @IBOutlet weak var rating: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15))
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
